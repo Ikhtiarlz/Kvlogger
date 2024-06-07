ee631e057139705e7225c2f56e62903fa4a39b0f
semalamTerbang <71621452+Ikhtiarlz@users.noreply.github.com>
2024-06-07 15:39:12 +0800

    update

---
+++ Student/Student/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Student
+//
+//  Created by STDC_09 on 07/06/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
